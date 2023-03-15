057f3c205288099bd2405156c404a5dcbfe5779f
Thabish <kadertabish2@gmail.com>
2023-03-15 12:12:05 +0400

    inital-config-client
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+function App() {
+	return (
+		<div className="App">
+			<p className="text-red-500 font-inter">Hello</p>
+		</div>
+	);
+}
+
+export default App;
